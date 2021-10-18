61c164c528f7ed41277731510063126ae0afe0fe
Artem <artem.kudrin70@gmail.com>
2021-10-18 22:59:04 +0300

    Добавил условие если нечего не выбрали.

--- lesson-1/fastfud/script.js
+++ lesson-1/fastfud/script.js
@@ -8,7 +8,7 @@ class GoodsItem {
       this.cal = cal;
     }
     render() {
-      return `<input type="radio" checked name="gender" id='${x++}'>${this.title}, Цена: ${this.price}, Калорий: ${this.cal}<br> `;
+      return `<input type="radio" name="gender" id='${x++}'>${this.title}, Цена: ${this.price}, Калорий: ${this.cal}<br> `;
     }
     renderDob() {
       return `<input type="checkbox" name="gender" id='${x++}'>${this.title}, Цена: ${this.price}, Калорий: ${this.cal}<br> `
@@ -38,45 +38,59 @@ class GoodsItem {
       ]
     };
     render() {
-      let listHtml = '<form>';
+      let listHtml = '';
       this.goods.forEach(good => {
         
         const goodItem = new GoodsItem(good.title, good.price, good.cal);
         listHtml += goodItem.render();
       });
-      document.querySelector('.item').innerHTML = ' <form class="goods-list"></form>\n</form><form class="goods-total"></form>\n</form><form class="goods-dop"></form>'
-      document.querySelector('.goods-list').innerHTML = listHtml + '</form>' ;
+      document.querySelector('.item').innerHTML = ' <form class="goods-list"></form></form><form class="goods-total"></form></form><form class="goods-dop"></form>'
+      document.querySelector('.goods-list').innerHTML = listHtml ;
     }
     renderTotal (){
-      let listFilling = '<form>';
+      let listFilling = '';
       this.filling.forEach(good => {
         const goodItem = new GoodsItem(good.title, good.price, good.cal);
         listFilling += goodItem.render();
       });
       
-      document.querySelector('.goods-total').innerHTML = listFilling + '</form>';
+      document.querySelector('.goods-total').innerHTML = listFilling;
     };
     renderDob (){
-      let listDop = '<form>';
+      let listDop = '';
       this.dop.forEach(good => {
         const goodItem = new GoodsItem(good.title, good.price, good.cal);
         listDop += goodItem.renderDob();
       });
       
-      document.querySelector('.goods-dop').innerHTML = listDop + '</form>';
+      document.querySelector('.goods-dop').innerHTML = listDop;
     }
     calculatePrice() {
       var praiceMas = this.goods.concat(this.filling, this.dop)
-      
       let f = document.querySelectorAll('input:checked')
+      let no = []
       for (let g=0;g<f.length;g++){
         let praiceId = f[g].id;
+        no[g] = [f[g].id]
         cena = cena + praiceMas[praiceId].price;
-        dop = dop + praiceMas[praiceId].cal;    
+        dop = dop + praiceMas[praiceId].cal; 
+        
       }
+      let x = true
+      if( no[0] > 1 || no[0] == undefined ){
+        alert('Выберите гамбургер')
+        x = false
+      }
+      if( no[1] > 4 || no[1] == undefined ){
+          alert('Выберите добавку')
+          x = false
+      }
+      if (x == true){
       document.querySelector('.praice').innerHTML = `<p>Цена: ${cena}<br> Калории: ${dop}</p>`
       cena = 0
       dop = 0
+      }
+        
     }
   }
    
